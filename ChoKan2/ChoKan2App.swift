0fdac3aa0f7029498f4c1ea9b670ef84e365b17e
TRAORE Rayan <rayan.traore03@gmail.com>
2022-03-24 14:12:56 +0100

    Initial Commit

---
+++ ChoKan2/ChoKan2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChoKan2App.swift
+//  ChoKan2
+//
+//  Created by apprenant72 on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChoKan2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoKan2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChoKan2
+//
+//  Created by apprenant72 on 24/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
